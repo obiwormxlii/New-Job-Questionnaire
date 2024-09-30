2866f563764dce1715a994ea49c041db0139c39f
Bailey Brayton <obiwan998@gmail.com>
2024-09-30 17:32:40 -0400

    put prerender into +layout.js instead of .svelte

--- svelte.config.js
+++ svelte.config.js
@@ -14,13 +14,10 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: 'index.html',
+			fallback: undefined,
 			precompress: false,
 			strict: true
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+		})
 	}
 };
 
